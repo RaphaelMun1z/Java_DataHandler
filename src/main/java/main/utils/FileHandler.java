db5bac60b9a0f67d898ff4aa40c369400b92c216
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-04-01 00:44:45 -0300

    feat: adiciona métodos auxiliares

--- src/main/java/main/utils/FileHandler.java
+++ src/main/java/main/utils/FileHandler.java
@@ -12,54 +12,61 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class FileHandler {
-	public static void extractFile(String zipFilePath, String targetFile, String destinationDir) throws IOException {
-		Path destDir = Paths.get(destinationDir);
-		if (!Files.exists(destDir))
-			Files.createDirectories(destDir);
+	public static void extractFile(String zipFilePath, String targetFile, String destinationDir) {
+		try {
+			System.out.println("Início do processo de extração do arquivo: " + targetFile);
 
-		try (ZipFile zipFile = new ZipFile(zipFilePath)) {
-			ZipEntry entry = zipFile.getEntry(targetFile);
-			if (entry == null)
-				throw new FileNotFoundException("Arquivo não encontrado na pasta compactada!");
+			// Diretório destino
+			Path destDir = Paths.get(destinationDir);
 
-			Path destPath = destDir.resolve(entry.getName());
-			try (InputStream is = zipFile.getInputStream(entry)) {
-				Files.copy(is, destPath, StandardCopyOption.REPLACE_EXISTING);
+			// Verifica se o diretório ainda não existe
+			if (!Files.exists(destDir))
+				Files.createDirectories(destDir); // Se ainda não existir, ele é criado
+
+			// Acessa o ZIP
+			try (ZipFile zipFile = new ZipFile(zipFilePath)) {
+				// Tenta capturar o arquivo dentro do ZIP
+				ZipEntry entry = zipFile.getEntry(targetFile);
+
+				// Quando não acha o arquivo no ZIP, o valor de 'entry' é null
+				if (entry == null)
+					throw new FileNotFoundException("Arquivo não encontrado na pasta compactada!");
+
+				// Cria o caminho para o arquivo extraído
+				Path destPath = destDir.resolve(entry.getName());
+				try (InputStream is = zipFile.getInputStream(entry)) {
+					Files.copy(is, destPath, StandardCopyOption.REPLACE_EXISTING);
+					System.out.println("Arquivo extraído com sucesso!");
+				}
 			}
+		} catch (IOException e) {
+			System.err.println("Ocorreu o seguinte erro: " + e.getMessage());
 		}
 	}
 
-	public static File getPdfFile(String filePath) {
-		if (filePath == null || filePath.trim().isEmpty())
-			throw new IllegalArgumentException("O caminho informado é inválido!");
+	public static File getFileWithExtension(String filePath, String extension) {
+		File targetFile = getTargetFile(filePath);
 
-		File targetFile = new File(filePath);
-
-		if (!targetFile.exists())
-			throw new IllegalArgumentException("O arquivo não foi encontrado!");
-
-		if (!targetFile.getName().endsWith(".pdf"))
-			throw new IllegalArgumentException("O arquivo não é um PDF");
-
-		return targetFile;
-	}
-
-	public static File getCsvFile(String filePath) {
-		if (filePath == null || filePath.trim().isEmpty())
-			throw new IllegalArgumentException("O caminho informado é inválido!");
-
-		File targetFile = new File(filePath);
-
-		if (!targetFile.exists())
-			throw new IllegalArgumentException("O arquivo não foi encontrado!");
-
-		if (!targetFile.getName().endsWith(".csv"))
-			throw new IllegalArgumentException("O arquivo não é um CSV");
+		if (!targetFile.getName().toLowerCase().endsWith(extension.toLowerCase()))
+			throw new IllegalArgumentException("O arquivo não é um " + extension.toUpperCase());
 
 		return targetFile;
 	}
 
 	public static void deleteFile(String filePath) {
+		try {
+			File targetFile = getTargetFile(filePath);
+
+			if (targetFile.delete())
+				System.out.println("Arquivo " + targetFile.getName() + " removido com sucesso!");
+			else
+				System.out.println("Erro: Não foi possível remover o arquivo " + targetFile.getName());
+		} catch (Exception e) {
+			System.out.println("Ocorreu o seguinte erro: " + e.getMessage());
+		}
+	}
+
+	private static File getTargetFile(String filePath) {
 		if (filePath == null || filePath.trim().isEmpty())
 			throw new IllegalArgumentException("O caminho informado é inválido!");
 
@@ -68,9 +75,6 @@ public class FileHandler {
 		if (!targetFile.exists())
 			throw new IllegalArgumentException("O arquivo não foi encontrado!");
 
-		if (targetFile.delete())
-			System.out.println("Arquivo " + targetFile.getName() + " removido com sucesso!");
-		else
-			throw new IllegalArgumentException("Não foi possível remover o arquivo!");
+		return targetFile;
 	}
 }
